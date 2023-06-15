8bc408a6c01dba353ec869a78467ab116dfb4e2d
smabdulmueid97 <smabdulmueid97@gmail.com>
2023-06-15 21:33:06 +0800

    init2

--- Sales_Record/orders_read.php
+++ Sales_Record/orders_read.php
@@ -1,145 +1,103 @@
-
-
-
 <?php
 include '../include/dbconnection.php';
 
-$query = "select * from orders";
+$query = "SELECT * FROM sales_record";
 
 if (isset($_GET['SearchBtn'])) {
     $searchBy = $_GET['SearchList'];
-    $search_text = $_GET['SearchTxt'];
+    $searchText = $_GET['SearchTxt'];
 
     if ($searchBy == "Order_Id") {
-        $query = "SELECT * from orders WHERE order_id = '$search_text'";
+        $query = "SELECT * FROM sales_record WHERE order_id = '$searchText'";
     } else if ($searchBy == "User_Id") {
-        $query = "SELECT * from orders WHERE user_id = '$search_text'";
+        $query = "SELECT * FROM sales_record WHERE customer_id = '$searchText'";
     } else if ($searchBy == "User_Phone") {
-        $query = "SELECT * from orders where user_phone = '$search_text'";
+        $query = "SELECT * FROM sales_record WHERE user_phone = '$searchText'";
     } else {
         echo "<script>alert('No Search Result Found.');</script>";
     }
-
 }
 
 $rows = mysqli_query($con, $query);
 $Total_Rows = mysqli_num_rows($rows);
-//echo $Total_Rows;
 ?>
 
-<!-- <!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Orders</title>
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-</head>
-
-<body> -->
-
-   <?php
+<?php
 include '../include/sidenav.php';
 ?>
 
-
-
-    <div class="container">
-
-
-        <div class="row mt-3">
-
-            <div class="col-md-3">
-
-            <a href="orders_add_new.php" class="btn btn-outline-dark">Add New Orders</a>
-
-            </div>
-
-            <div class="col-md-8">
-
-                <form action="" method="get" class="form-inline">
-
-                    <select name="SearchList" order_id="" required class="form-control">
-                        <option value="">Select Field</option>
-                        <option value="Order_Id">Order Id</option>
-                        <option value="User_Id">User Id</option>
-                        <option value="User_Phone">User Phone</option>
-                    </select>
-                    <input type="text" placeholder="Search By " name="SearchTxt" required class="form-control">
-
-                    <input type="submit" value="Search" name="SearchBtn" class="btn btn-primary">
-                    <a href="orders_read.php" class="btn btn-danger">Reset</a>
-
-
-                </form>
-
-            </div>
-
+<div class="container">
+    <div class="row mt-3">
+        <div class="col-md-3">
+            <a href="orders_add_new.php" class="btn btn-outline-dark">Add New sales_record</a>
         </div>
-
-        <div class="row mt-3">
-
-            <div class="col-md-12">
-
-    <?php
-
-if ($Total_Rows != 0) {
-    ?>
-
-    <table class="table table-bordered table-striped table-hover table-borderless">
-        <thead>
-        <tr>
-            <th colspan="8" class="text-center">Orders</th>
-        </tr>
-        <tr>
-            <th>Order Id</th>
-            <th>Order Cost</th>
-            <th>User Address</th>
-            <th>User Phone</th>
-            <th>Order Date</th>
-            <th>User City</th>
-            <th>User Id</th>
-            <th>Order Status</th>
-        </tr>
-    </thead>
-    <?php
-echo "<tbody>";
-    while ($data = mysqli_fetch_assoc($rows)) {
-        echo "<tr>
-              <td>" . $data["order_id"] . "</td>
-              <td>" . $data["order_cost"] . "</td>
-              <td>" . $data["user_address"] . "</td>
-              <td>" . $data["user_phone"] . "</td>
-              <td>" . $data["order_date"] . "</td>
-              <td>" . $data["user_city"] . "</td>
-              <td>" . $data["user_id"] . "</td>
-              <td>" . $data["order_status"] . "</td>
-
-            <td><a href='orders_view.php?order_id=$data[order_id]' class='btn btn-success'>View</a></td>
-            <td><a href='orders_update.php?order_id=$data[order_id]' class='btn btn-info'>Edit</a></td>
-
-        </tr>";
-    }
-    echo "</tbody>";
-} else {
-    echo "No Rows Found";
-}
-
-?>
-    </table>
-        </div>
+        <div class="col-md-8">
+            <form action="" method="get" class="form-inline">
+                <select name="SearchList" order_id="" required class="form-control">
+                    <option value="">Select Field</option>
+                    <option value="Order_Id">Order Id</option>
+                    <option value="User_Id">User Id</option>
+                    <option value="User_Phone">User Phone</option>
+                </select>
+                <input type="text" placeholder="Search By" name="SearchTxt" required class="form-control">
+                <input type="submit" value="Search" name="SearchBtn" class="btn btn-primary">
+                <a href="orders_read.php" class="btn btn-danger">Reset</a>
+            </form>
         </div>
     </div>
+    <div class="row mt-3">
+        <div class="col-md-12">
+            <?php
+            if ($Total_Rows != 0) {
+            ?>
+                <table class="table table-bordered table-striped table-hover table-borderless">
+                    <thead>
+                        <tr>
+                            <th colspan="8" class="text-center">sales_record</th>
+                        </tr>
+                        <tr>
+                            <th>Sales Record ID</th>
+                            <th>Employee ID</th>
+                            <th>Customer ID</th>
+                            <th>Sales Record Date</th>
+                            <th>Sales Record Time</th>
+                            <th>Sales Record Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        while ($data = mysqli_fetch_assoc($rows)) {
+                            echo "<tr>
+                            <td>" . $data["sales_record_id"] . "</td>
+                            <td>" . $data["employee_id"] . "</td>
+                            <td>" . $data["customer_id"] . "</td>
+                            <td>" . $data["sales_record_date"] . "</td>
+                            <td>" . $data["sales_record_time"] . "</td>
+                            <td>" . $data["sales_record_price"] . "</td>
+                            <td><a href='orders_view.php?sales_record_id=$data[sales_record_id]' class='btn btn-success'>View</a></td>
+            <td><a href='orders_update.php?sales_record_id=$data[sales_record_id]' class='btn btn-info'>Edit</a></td>
+                        </tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            <?php
+            } else {
+                echo "No Rows Found";
+            }
+            ?>
+        </div>
+    </div>
+</div>
 
-    <script>
+<script>
+    function Confirmation() {
+        return confirm("Are You Sure To Delete Data");
+    }
+</script>
 
-    function Confirmation()
-        {
-            return confirm("Are You Sure To Delete Data");
-        }
-
-    </script>
-
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </body>
+
 </html>
