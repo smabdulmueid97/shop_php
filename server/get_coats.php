9cf2b37cc930570067254d18ffa8d5eef2558e12
smabdulmueid97 <smabdulmueid97@gmail.com>
2023-06-16 22:14:00 +0800

    init12

--- server/get_coats.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include 'connection.php';
-$stmt = $conn->prepare("SELECT * FROM products WHERE product_category ='coats' LIMIT 4");
-
-$stmt->execute();
-$cotes_products = $stmt->get_result();//[]
